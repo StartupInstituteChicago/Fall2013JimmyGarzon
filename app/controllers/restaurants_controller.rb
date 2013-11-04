cd14677e69bbab74131ba860a7a82b295b8262e6
Jimmy Garzon <jimmygarzon@gmail.com>
2013-11-03 22:06:47 -0800

    commit without bootstrap, it includes carrierwave uploads and gmaps

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,51 @@
+class RestaurantsController < ApplicationController
+	def new
+		@restaurant = Restaurant.new
+	end
+
+	def create
+		@restaurant = Restaurant.new(restaurant_params)
+		
+		if @restaurant.save
+			redirect_to @restaurant
+		else
+			render 'new'
+		end
+	end
+
+	def show
+		@restaurant = Restaurant.find(params[:id])
+		@full_address = @restaurant.street + ' ' + @restaurant.city + ' ' +@restaurant.state + ' ' + @restaurant.zipcode
+	end
+
+
+	def index
+		@restaurants = Restaurant.find(:all)
+	end
+
+	def edit
+		@restaurant = Restaurant.find(params[:id])
+	end
+
+	def update
+		@restaurant = Restaurant.find(params[:id])
+
+		if @restaurant.update(params[:restaurant].permit(:name, :description, :street, :city, :state, :zipcode, :phone, :image, :remote_image_url, :menu))
+			redirect_to @restaurant
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@restaurant = Restaurant.find(params[:id])
+		@restaurant.destroy
+
+		redirect_to restaurants_path		
+	end
+
+	private
+		def restaurant_params
+			params.require(:restaurant).permit(:name, :description, :street, :city, :state, :zipcode, :phone, :image, :remote_image_url, :menu)
+		end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RestaurantApp::Application.config.secret_key_base = 'dd9aecd56e26929c6c113294f7e155590c4d77ce4c65c976bcbb6c2bd17c4fb2f5bceb86fcb36ce12e3d158caf3c7f6429bf080bd995a028d3d0dc4d76644a66'
