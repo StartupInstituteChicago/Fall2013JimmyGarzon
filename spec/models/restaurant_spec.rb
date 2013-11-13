88635f8c035863027599358b1a313b3e6f3f2cfa
Jimmy Garzon <jimmygarzon@gmail.com>
2013-11-13 10:03:32 -0800

    added some tests

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
-describe Restaurant do
-	describe "full_adress" do
-		it "concatenates the address fields" do
-			restaurant = FactoryGirl.build(:restaurant)
+	describe Restaurant do
+		describe "full_adress" do
+			it "concatenates the address fields" do
+				restaurant = FactoryGirl.build(:restaurant)
 
-			expect(restaurant.full_address).to eq "1 Main Springfield IL 60452"
+				expect(restaurant.full_address).to eq "1 Main Springfield IL 60452"
 
-		end
-end
+			end
+	end
 	
 end
