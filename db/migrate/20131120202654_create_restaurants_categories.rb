b15ee3c24019d85c005d9b3e478b0cd163ae7f4e
jimbog <jimmygarzon@gmail.com>
2013-11-21 16:29:06 +0000

    fixed habtm and db

--- db/migrate/20131120202654_create_restaurants_categories.rb
+++
@@ -1,8 +0,0 @@
-class CreateRestaurantsCategories < ActiveRecord::Migration
-  def change
-    create_table :restaurants_categories, :id => false do |t|
-      t.references :category
-      t.references :restaurant
-    end
-  end
-end
